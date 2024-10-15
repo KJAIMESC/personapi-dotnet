42a3112a6d8737f8b31e4be5c58767ccf2c251ac
Kevin Jaimes Carrillo <104458684+KJAIMESC@users.noreply.github.com>
2024-10-15 00:34:39 -0500

    Entities Created

--- personapi-dotnet/obj/Debug/net8.0/personapi-dotnet.AssemblyInfo.cs
+++ personapi-dotnet/obj/Debug/net8.0/personapi-dotnet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("personapi-dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c84e41667999808d22b7ec78253518fa71aa46f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af61cc0546f3f5739078216eb508acc91b3ae3e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("personapi-dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("personapi-dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
